49ca7963a9b21ec7578277801ff6a985014b421e
tanggyuri <xorb0835@naver.com>
2023-05-25 17:52:24 +0900

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import "./style.scss";
+import HeroBanner from "./heroBanner/HeroBanner";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
+import Trending from "./trending/Trending";
+
+const Home = () => {
+    return (
+        <div className="homePage">
+            <HeroBanner />
+            <Trending />
+            <Popular />
+            <TopRated />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import "./style.scss";
+import Img from "../../../components/lazyLoadImage/Img";
+
+import useFetch from "../../../hooks/useFetch";
+import { useSelector } from "react-redux";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+
+const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const { data, loading } = useFetch("/movie/upcoming");
+  const { url } = useSelector((state) => state.home);
+
+  useEffect(() => {
+    const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+  return (
+    <div className="heroBanner">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
+      <ContentWrapper>
+        <div className="opacity-layer"></div>
+        <div className="wrapper">
+          <div className="heroBannerContent">
+            <span className="title">Welcome.</span>
+            <span className="subTitle">Millions of movies, TV shows and people to discover. Explore now.</span>
+            <div className="searchInput">
+              <input type="text" placeholder="Search for a movie or tv show..." onChange={(e) => setQuery(e.target.value)} onKeyUp={searchQueryHandler} />
+              <button>Search</button>
+            </div>
+          </div>
+        </div>
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default HeroBanner;
